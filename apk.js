804f37cf6dc83d1bf1ff92bf2585c031a8c30908
Pratheev <pratheev@email.com>
2017-07-07 19:02:47 +0000

    Fixed Bug

--- apk.js
+++ apk.js
@@ -3,13 +3,9 @@
 const apk = require('commander');
 const chalk = require('chalk');
 const Fetch = require('./fetch');
-const jsonfile = require('jsonfile')
-var version="";
 
-jsonfile.readFile('package.json', function(err, obj) {
-  version = obj.version;
   apk
-  .version(chalk.bold.green(version))
+  .version(chalk.bold.green("1.0.1"))
   .description(chalk.bold.blue('Download APKs from CLI'))
   .usage(chalk.bold.green('<package_name> ') + chalk.dim.cyan('[options]'))
   .parse(process.argv);
@@ -17,4 +13,3 @@ jsonfile.readFile('package.json', function(err, obj) {
   if (!apk.args.length) apk.help();
   else if(apk.args.length>1) console.log(chalk.bold.red("One at a Time!"));
   else Fetch.getDetails(apk);
-});
