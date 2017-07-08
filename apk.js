4c7873f800fe678bd11371aa1fe1cd81e5b1f41b
Pratheev <pratheev@email.com>
2017-07-08 18:31:49 +0000

    Migrated to APKPURE

--- apk.js
+++ apk.js
@@ -1,15 +1,3 @@
 #!/usr/bin/env node
 
-const apk = require('commander');
-const chalk = require('chalk');
-const Fetch = require('./fetch');
-
-  apk
-  .version(chalk.bold.green("1.1.1"))
-  .description(chalk.bold.blue('Download APKs from CLI'))
-  .usage(chalk.bold.green('<package_name> ') + chalk.dim.cyan('[options]'))
-  .parse(process.argv);
-  
-  if (!apk.args.length) apk.help();
-  else if(apk.args.length>1) console.log(chalk.bold.red("One at a Time!"));
-  else Fetch.getDetails(apk);
+require('./lib/cli');
